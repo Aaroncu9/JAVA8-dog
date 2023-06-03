21320aa1623ef2841dedc1b9db412832ca1e1244
mundo9527 <Mundo_123456>
2023-06-03 20:58:08 +0800

    filter、map、sort

--- src/main/java/com/stream/StreamDemo.java
+++ src/main/java/com/stream/StreamDemo.java
@@ -1,10 +1,11 @@
 package com.stream;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Stream;
+import java.util.Map.*;
 
 /**
  * @Author: liuchuanlong
@@ -13,18 +14,67 @@ import java.util.function.Predicate;
 public class StreamDemo {
 
     public static void main(String[] args) {
+        test6();
+        // test5();
+        // test4();
+        // test3();
+        // test2();
+        // test(authors);
+    }
 
-        List<Author> authors = getAuthors();
+    private static void test(List<Author> authors) {
         authors.stream()
                 .distinct()
                 .filter(author -> author.getAge() < 18)
                 .forEach(author -> System.out.println(author.getName()));
     }
 
+    public static void test2() {
+        Integer[] arr = {1,2,3,4,5};
+        Stream<Integer> stream = Arrays.stream(arr);
+        stream.distinct()
+                .filter(index -> index>2)
+                .forEach(System.out::println);
+    }
+
+    public static void test3() {
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("小黑", 19);
+        map.put("小白", 17);
+        map.put("小虎", 16);
+
+        map.entrySet().stream().filter(stringIntegerEntry -> stringIntegerEntry.getValue() >17).forEach(System.out::println);
+    }
+
+    public static void test4() {
+        List<Author> authors = getAuthors();
+        authors.stream()
+                .filter(author -> author.getName().length()>2)
+                .forEach(System.out::println);
+    }
+
+    public static void test5() {
+        List<Author> authors = getAuthors();
+        authors.stream().map(Author::getName)
+                .forEach(System.out::println);
+    }
+
+    public static void test6() {
+        List<Author> authors = getAuthors();
+        authors.stream()
+                .distinct()
+                .sorted(new Comparator<Author>() {
+                    @Override
+                    public int compare(Author o1, Author o2) {
+                        return o1.getAge() > o2.getAge() ? 1:0;
+                    }
+                })
+                .forEach(author -> System.out.println(author.getName()));
+    }
     public static List<Author> getAuthors() {
         // 数据初始化
         Author author1 = new Author(1L, "蒙多", 33, "一个从菜刀中明悟真理的祖安人", null);
-        Author author2 = new Author(2L, "亚索", 15, "死亡如风，常伴吾身", null);
+        Author author2 = new Author(2L, "亚索", 1, "死亡如风，常伴吾身", null);
         Author author3 = new Author(3L, "易大师", 14, "一击几盏", null);
         Author author4 = new Author(3L, "易大师", 14, "一击几盏", null);
 
